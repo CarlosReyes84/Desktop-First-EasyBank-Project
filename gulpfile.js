1df957d3e64a796670d8d482d8421e8bf19d43cf
CarlosReyes <carlosreyes8436@gmail.com>
2021-05-10 19:30:46 -0600

    All bugs fixed!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, watch } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-dart-sass');
+const browser = require('browser-sync')
 
 //FUNCTION TO COMPILE SASS
 function css () {
@@ -11,7 +12,15 @@ function css () {
 
 //FUNCTION TO WATCH FOR ANY CHANGES AND THEN APPLY CSS FUCTION
 function watching() {
+    browser.init({
+        server: {
+            baseDir: './'
+        }
+    })
+
     watch('src/scss/**/*.scss', css)
+    watch('./*.html').on('change', browser.reload);
+    watch('build/css/**/*.css').on('change', browser.reload);
 }
 
 exports.default = watching;
